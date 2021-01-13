5e9dac67256dd2b1766ea30934854c2c0276c39a
Andy B <andybarnov@gmail.com>
2021-01-13 14:25:47 +0100

    wip

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,11 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const environment = require("./environment");
 
-const path = require("path");
-
-environment.config.devServer.watchContentBase = true;
-environment.config.devServer.contentBase = [
-  path.join(__dirname, "../../app/components"),
-];
-
 module.exports = environment.toWebpackConfig();
